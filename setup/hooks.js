dbc3b39f42b608788eafe3b1e1031168687c2c36
Damien O'Sullivan <damosull@damiens-mbp.lan>
2023-10-27 10:04:48 +0100

    fixing hooks

--- setup/hooks.js
+++ setup/hooks.js
@@ -1,14 +1,16 @@
 const playwright = require('playwright');
-const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');
+const {
+  After,
+  AfterAll,
+  AfterStep,
+  Before,
+  BeforeAll,
+  Status,
+} = require('@cucumber/cucumber');
 
 BeforeAll(async () => {
   console.log('Launch Browser');
-  global.browser = await playwright['chromium'].launch({ headless: false });
-});
-
-AfterAll(async () => {
-  console.log('Close Browser');
-  await global.browser.close();
+  global.browser = await playwright['chromium'].launch({ headless: true });
 });
 
 Before(async () => {
@@ -17,8 +19,31 @@ Before(async () => {
   global.page = await global.context.newPage();
 });
 
-After(async () => {
+After(async function ({ pickle, result }) {
+  // Screenshot only for failure
+  if (result?.status == Status.FAILED) {
+    const image = await global.page.screenshot({
+      path: `./test-results/screenshots/${pickle.name}.png`,
+      type: 'png',
+    });
+    await this.attach(image, 'image/png');
+  }
+
+  // Screenshot after each scenario
+  // const image = await global.page.screenshot({ path: `./test-results/screenshots/${pickle.name}.png`, type: "png" });
+  // await this.attach(image, "image/png");
   console.log('Close context and page');
   await global.page.close();
   await global.context.close();
 });
+
+AfterAll(async () => {
+  console.log('Close Browser');
+  await global.browser.close();
+});
+
+// AfterStep(async function({pickle}){
+//     // Screenshot after each step
+//     const image = await global.page.screenshot({ path: `./test-results/screenshots/${pickle.name}.png`, type: "png" });
+//     await this.attach(image, "image/png");
+// })
